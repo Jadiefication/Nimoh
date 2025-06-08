8b7636b13a8c319f082c2b7897fba24d274df5d7
Jadiefication <jadeczjade008@gmail.com>
2025-06-08 21:13:32 +0200

    feat: add base tree calculation

--- src/main/kotlin/io/main/gen/tree/TreeGen.kt
+++ src/main/kotlin/io/main/gen/tree/TreeGen.kt
@@ -1,8 +1,11 @@
 package io.main.gen.tree
 
+import org.bukkit.Material
+import org.bukkit.block.data.type.PointedDripstone
 import org.bukkit.generator.BlockPopulator
 import org.bukkit.generator.LimitedRegion
 import org.bukkit.generator.WorldInfo
+import org.joml.Vector3L
 import java.util.Random
 
 class TreeGen: BlockPopulator() {
@@ -16,6 +19,35 @@ class TreeGen: BlockPopulator() {
         chunkZ: Int,
         limitedRegion: LimitedRegion
     ) {
-        limitedRegion
+        for (x in 0..16) {
+            for (z in 0..16) {
+                val worldX = chunkX * 16 + x
+                val worldZ = chunkZ * 16 + z
+                if (random.nextInt(5) == 1) {
+                    val worldY = handleGettingFreeBlock(worldInfo, worldX, worldZ, limitedRegion)
+                    generateFractalTree(Vector3L(worldX, worldY, worldZ), Vector3L(0, 5, 0), 3, limitedRegion, random, 0)
+                }
+            }
+        }
+    }
+
+    private fun handleGettingFreeBlock(worldInfo: WorldInfo, x: Int, z: Int, limitedRegion: LimitedRegion): Int {
+        var freeBlock = worldInfo.maxHeight
+        for (y in 0..freeBlock) {
+            if (limitedRegion.getBlockData(x, y, z).material == Material.AIR && (
+                        limitedRegion.getBlockData(x, y - 1, z).material == Material.GRASS_BLOCK || limitedRegion.getBlockData(x, y - 1, z).material == Material.DIRT)) {
+                freeBlock = y
+            } else {
+                continue
+            }
+        }
+        return freeBlock
+    }
+
+    private fun generateFractalTree(basePos: Vector3L, direction: Vector3L, thickness: Int, limitedRegion: LimitedRegion, random: Random, iterationDepth: Int) {
+        if (direction.length() < 1 || iterationDepth >= maxDepth) {
+
+            return
+        }
     }
 }
