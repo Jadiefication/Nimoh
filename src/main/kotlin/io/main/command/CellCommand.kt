953f45d06677714f25b475268b4986303b31e267
Jadiefication <jadeczjade008@gmail.com>
2025-06-09 15:27:16 +0200

    refactor!: move command to paper command

--- src/main/kotlin/io/main/command/CellCommand.kt
+++ src/main/kotlin/io/main/command/CellCommand.kt
@@ -1,61 +1,72 @@
 package io.main.command
 
+import com.mojang.brigadier.Command
+import com.mojang.brigadier.arguments.DoubleArgumentType
+import com.mojang.brigadier.arguments.IntegerArgumentType
+import com.mojang.brigadier.arguments.LongArgumentType
+import com.mojang.brigadier.arguments.StringArgumentType
+import com.mojang.brigadier.builder.LiteralArgumentBuilder
 import io.main.gen.WorldGen
+import io.papermc.paper.command.brigadier.CommandSourceStack
+import io.papermc.paper.command.brigadier.Commands
 import net.kyori.adventure.text.Component
 import org.bukkit.Location
-import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
 class CellCommand(
     val worldGen: WorldGen
-): Command("command") {
+) {
 
-    override fun execute(
-        sender: CommandSender,
-        commandLabel: String,
-        args: Array<out String>
-    ): Boolean {
-        if (sender.isOp) {
-            try {
-                when (args.first()) {
-                    "getIslandCells" -> {
-                        sender.sendMessage {
-                            Component.text(worldGen.islandInCell.keys.toString())
-                        }
-                    }
-                    "tp" -> {
-                        val cellX = args[1].toLong()
-                        val cellZ = args[2].toLong()
-                        handleTpToCell(cellX, cellZ, sender)
+    fun command(): LiteralArgumentBuilder<CommandSourceStack> {
+        return Commands.literal("cell")
+            .then(Commands.literal("getIslandCells").executes {
+                val sender = it.source.executor
+
+                if (sender?.isOp == true) {
+                    sender.sendMessage {
+                        Component.text(worldGen.islandInCell.keys.toString())
                     }
                 }
-                return true
-            } catch (e: Exception) {
-                sender.sendMessage {
-                    Component.text("No args supplied")
-                }
-                return false
-            }
-        } else {
-            return false
-        }
+
+                return@executes Command.SINGLE_SUCCESS
+            }).then(Tp().command())
     }
 
-    private fun handleTpToCell(cellX: Long, cellZ: Long, sender: CommandSender) {
-        if (sender !is Player) {
-            sender.sendMessage {
-                Component.text("Only players may use this command")
-            }
-            return
+    inner class Tp {
+
+        fun command(): LiteralArgumentBuilder<CommandSourceStack> {
+            return Commands.literal("tp")
+                .then(Commands.argument("x", LongArgumentType.longArg())
+                    .then(Commands.argument("z", LongArgumentType.longArg()).executes {
+                        val x = LongArgumentType.getLong(it, "x")
+                        val z = LongArgumentType.getLong(it, "z")
+                        val sender = it.source.executor
+
+                        if (sender?.isOp == true) {
+                            handleTpToCell(x, z, sender)
+                        }
+
+                        return@executes Command.SINGLE_SUCCESS
+                    })
+                )
         }
-        if (worldGen.islandInCell.contains((cellX to cellZ))) {
-            val (centerX, centerZ) = worldGen.islandInCell[(cellX to cellZ)]!!
-            sender.teleport(Location(sender.world, centerX.toDouble(), sender.y, centerZ.toDouble()))
-        } else {
-            val centerX = cellX * worldGen.cellSize + worldGen.cellSize / 2
-            val centerZ = cellZ * worldGen.cellSize + worldGen.cellSize / 2
-            sender.teleport(Location(sender.world, centerX.toDouble(), sender.y, centerZ.toDouble()))
+
+        private fun handleTpToCell(cellX: Long, cellZ: Long, sender: CommandSender) {
+            if (sender !is Player) {
+                sender.sendMessage {
+                    Component.text("Only players may use this command")
+                }
+                return
+            }
+            if (worldGen.islandInCell.contains((cellX to cellZ))) {
+                val (centerX, centerZ) = worldGen.islandInCell[(cellX to cellZ)]!!
+                sender.teleport(Location(sender.world, centerX.toDouble(), sender.y, centerZ.toDouble()))
+            } else {
+                val centerX = cellX * worldGen.cellSize + worldGen.cellSize / 2
+                val centerZ = cellZ * worldGen.cellSize + worldGen.cellSize / 2
+                sender.teleport(Location(sender.world, centerX.toDouble(), sender.y, centerZ.toDouble()))
+            }
         }
     }
 }
--- src/main/kotlin/io/main/nimoh/Nimoh.kt
+++ src/main/kotlin/io/main/nimoh/Nimoh.kt
@@ -1,6 +1,9 @@
 package io.main.nimoh
 
+import io.main.command.CellCommand
 import io.main.gen.WorldGen
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
+import org.bukkit.Bukkit
 import org.bukkit.generator.ChunkGenerator
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -24,6 +27,10 @@ class Nimoh : JavaPlugin() {
                 6 -> worldGen.cellSize = config.getInt(value, worldGen.cellSize)
             }
         }
+
+        this.lifecycleManager.registerEventHandler(LifecycleEvents.COMMANDS, {
+            it.registrar().register(CellCommand(worldGen).command().build())
+        })
     }
 
     override fun onDisable() {
